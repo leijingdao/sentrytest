2511a9a9e209c874211d49d920ef569ac875fe5f
Leijd <leijd@akulaku.com>
2019-12-12 20:18:47 +0800

    故意报错

--- src/components/TT.js
+++ src/components/TT.js
@@ -3,36 +3,14 @@ import {View, Text, Button} from 'react-native';
 import * as Sentry from '@sentry/react-native';
 
 export default class T extends React.PureComponent {
-  report = () => {
-    throw new Error('wefwefqfeqwfqweef');
-  };
-
-  reportNaitive = () => {
-    // throw new Error('My first Sentry error!');
-    Sentry.nativeCrash();
-  };
-
-  reportCaptureException = () => {
-    // try {
-    //   a.length();
-    // } catch (error) {
-    //   Sentry.captureException(error);
-    // }
-    wefwefqfeqwfqweef.length();
-  };
-
-  ccc = () => {
-    Promise.reject('wefwefqfeqwfqweef');
-  };
-
   render() {
     return (
       <View>
         <Text>测试报错</Text>
-        <Button title="trigger a js exception" onPress={this.report} />
+        {/* <Button title="trigger a js exception" onPress={this.report} />
         <Button title="trigger a native crash" onPress={this.reportNaitive} />
         <Button title="try catch" onPress={this.reportCaptureException} />
-        <Button title="fsdfsdf" onPress={this.ccc} />
+        <Button title="fsdfsdf" onPress={this.ccc} /> */}
 
         <Button
           title="trigger a js exception"
@@ -48,11 +26,21 @@ export default class T extends React.PureComponent {
             // } catch (error) {
             //   Sentry.captureException(error);
             // }
-            vvdsvsdfg.length();
+            console.log(fjowiefnwoief);
           }}
         />
-        <Button title="try catch" onPress={this.reportCaptureException} />
-        <Button title="fsdfsdf" onPress={this.ccc} />
+        <Button
+          title="try catch"
+          onPress={() => {
+            fjwoiefhnoweinfiowef;
+          }}
+        />
+        <Button
+          title="fsdfsdf"
+          onPress={() => {
+            let a = 1 / 0 ;
+          }}
+        />
       </View>
     );
   }
